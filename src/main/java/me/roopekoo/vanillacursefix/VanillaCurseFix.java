28bdbab83f6879937472c348f6786d9832ecec58
Roope Korkee <roope.korkee@tuni.fi>
2021-06-16 13:09:32 +0300

    Reformat code

--- src/main/java/me/roopekoo/vanillacursefix/VanillaCurseFix.java
+++ src/main/java/me/roopekoo/vanillacursefix/VanillaCurseFix.java
@@ -12,37 +12,34 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VanillaCurseFix extends JavaPlugin implements Listener {
 
-    @Override
-    public void onEnable() {
-        System.out.println("VanillaCurseFix starting up.");
-        Bukkit.getServer().getPluginManager().registerEvents(this, this);
-    }
+	@Override public void onEnable() {
+		System.out.println("VanillaCurseFix starting up.");
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+	}
 
-    @Override
-    public void onDisable() {
-        System.out.println("VanillaCurseFix shut down.");
-    }
+	@Override public void onDisable() {
+		System.out.println("VanillaCurseFix shut down.");
+	}
 
-    @EventHandler
-    public void onDeath(PlayerDeathEvent event) {
-        Player player = event.getEntity();
-        World world = player.getWorld();
+	@EventHandler public void onDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		World world = player.getWorld();
 
-        for (int i = 0; i < player.getInventory().getSize(); i++) {
-            ItemStack item = player.getInventory().getItem(i);
-            if (item == null) {
-                continue;
-            }
-            if (item.containsEnchantment(Enchantment.VANISHING_CURSE)) {
-                player.getInventory().clear(i);
-                continue;
-            }
-            if ((i >= 36) && (i < 40)) {
-                if (item.containsEnchantment(Enchantment.BINDING_CURSE)) {
-                    player.getInventory().clear(i);
-                    world.dropItem(player.getLocation(), item).setOwner(player.getUniqueId());
-                }
-            }
-        }
-    }
+		for(int i = 0; i<player.getInventory().getSize(); i++) {
+			ItemStack item = player.getInventory().getItem(i);
+			if(item == null) {
+				continue;
+			}
+			if(item.containsEnchantment(Enchantment.VANISHING_CURSE)) {
+				player.getInventory().clear(i);
+				continue;
+			}
+			if((i>=36) && (i<40)) {
+				if(item.containsEnchantment(Enchantment.BINDING_CURSE)) {
+					player.getInventory().clear(i);
+					world.dropItem(player.getLocation(), item).setOwner(player.getUniqueId());
+				}
+			}
+		}
+	}
 }
